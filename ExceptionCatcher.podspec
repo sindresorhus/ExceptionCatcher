9bd161653fc781bb13713085983cd31b097ddfd2
Ali <alikhajepur@gmail.com>
2023-07-05 10:15:11 +0330

    #feature: support Cocopod

---
+++ ExceptionCatcher.podspec
@@ -0,0 +1,18 @@
+Pod::Spec.new do |spec|
+  spec.name             = 'ExceptionCatcher'
+  spec.version          = '2.0.0'
+  spec.summary          = 'The ability to catch exceptions should really be built into Swift.'
+  spec.description      = 'There are many Cocoa APIs that can throw exceptions that cannot be caught in Swift (NSKeyedUnarchiver, NSTask, NSObject, etc). This package wraps an Objective-C exception handler to make it possible to catch such exceptions.'
+
+  spec.homepage         = 'https://github.com/Ali72/ExceptionCatcher'
+  spec.license          = { :type => 'MIT', :file => 'LICENSE' }
+  spec.authors          = {
+    'sindresorhus' => 'sindresorhus@gmail.com'
+    'Ali Khajehpour' => 'alikhajepur@gmail.com'
+  }
+  spec.source           = { :git => 'https://github.com/Ali72/ExceptionCatcher', :tag => spec.version.to_s }
+
+  spec.ios.deployment_target = '10.0'
+  spec.source_files     = 'Sources/**/*'
+  spec.swift_version    = '5.5'
+end
--- Sources/ExceptionCatcher/ExceptionCatcher.swift
+++ Sources/ExceptionCatcher/ExceptionCatcher.swift
@@ -1,47 +1,49 @@
 @_implementationOnly import Foundation
+#if SWIFT_PACKAGE
 @_implementationOnly import Internal
+#endif
 
 public enum ExceptionCatcher {
-	/**
-	Catch a Objective-C exception.
+    /**
+    Catch a Objective-C exception.
 
-	- Returns: The value returned from the given callback.
+    - Returns: The value returned from the given callback.
 
-	```swift
-	import Foundation
-	import ExceptionCatcher
+    ```swift
+    import Foundation
+    import ExceptionCatcher
 
-	final class Foo: NSObject {}
+    final class Foo: NSObject {}
 
-	do {
-		let value = try ExceptionCatcher.catch {
-			return Foo().value(forKey: "nope")
-		}
+    do {
+        let value = try ExceptionCatcher.catch {
+            return Foo().value(forKey: "nope")
+        }
 
-		print("Value:", value)
-	} catch {
-		print("Error:", error.localizedDescription)
-		//=> Error: The operation couldn’t be completed. [valueForUndefinedKey:]: this class is not key value coding-compliant for the key nope.
-	}
-	```
-	*/
-	@discardableResult
-	public static func `catch`<T>(callback: () throws -> T) throws -> T {
-		var returnValue: T!
-		var returnError: Error?
+        print("Value:", value)
+    } catch {
+        print("Error:", error.localizedDescription)
+        //=> Error: The operation couldn’t be completed. [valueForUndefinedKey:]: this class is not key value coding-compliant for the key nope.
+    }
+    ```
+    */
+    @discardableResult
+    public static func `catch`<T>(callback: () throws -> T) throws -> T {
+        var returnValue: T!
+        var returnError: Error?
 
-		try _ExceptionCatcher.catchException {
-			do {
-				returnValue = try callback()
-			} catch {
-				returnError = error
-			}
-		}
+        try _ExceptionCatcher.catchException {
+            do {
+                returnValue = try callback()
+            } catch {
+                returnError = error
+            }
+        }
 
-		if let returnError {
-			throw returnError
-		}
+        if let returnError {
+            throw returnError
+        }
 
-		return returnValue
-	}
+        return returnValue
+    }
 }
